696cf71cebf68c3ee2372bea8e55f04fb650176e
Abey Onalaja <Abey.onalaja@gmail.com>
2019-12-22 21:21:00 +0000

    tuples

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,21 @@ fn main() {
     x += 1;
 
     println!("x is {}", x);
+
+    let a = true;
+    let b = false;
+
+    if a {
+        println!("a is true");
+    }
+
+    if b {
+        println!("b is true");
+    }
+
+    let tup = (1, 'c', true);
+    let first = tup.0;
+    let second = tup.1;
+
+    println!("the first is {} and the second is {}", first, second);
 }
