a74a6f1f23015854649a0e6b9db429747bbf5c65
Abey Onalaja <Abey.onalaja@gmail.com>
2019-12-23 01:08:52 +0000

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn next_birthday(name: &str, current_age: u8) {
+    let next_age = current_age + 1;
+    println!(
+        "Hi {}, on your next birthday, you'll be {}!",
+        name, next_age
+    );
+}
+
+fn square(num: i32) -> i32 {
+    num * num
+}
+
 fn main() {
     let mut x = 5;
     let y = 6;
@@ -25,4 +37,6 @@ fn main() {
     let second = tup.1;
 
     println!("the first is {} and the second is {}", first, second);
+    next_birthday("Bob", 34);
+    println!("The answer is {}", square(32));
 }
