4a2172cf4939724638cd8a56355711212abeec2a
Abey Onalaja <Abey.onalaja@gmail.com>
2019-12-23 01:55:47 +0000

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+enum HockeyPosition {
+    Center,
+    Wing,
+    Defense,
+    Goalie,
+}
+
+enum Clock {
+    Sundial(u8),
+    Digital(u8, u8),
+    Analog(u8, u8, u8),
+}
+
+fn tell_time(clock: Clock) {
+    match clock {
+        Clock::Sundial(hours) => println!("It is about {} o'clock", hours),
+        Clock::Digital(hours, minutes) => println!("It is {} minutes past {} ", minutes, hours),
+        Clock::Analog(hours, minutes, seconds) => {
+            println!(
+                "It is {} minutes and {} seconds past {} 0'clock",
+                minutes, seconds, hours
+            );
+        }
+    }
+}
+
+fn next_player(position: HockeyPosition) {}
+
 fn main() {
+    let position = HockeyPosition::Defense;
+    next_player(position);
     for i in 1..11 {
         println!("Now serving number {}", i);
     }
@@ -10,4 +40,6 @@ fn main() {
         3 => println!("Three is a crowd"),
         _ => println!("Some other number"),
     }
+
+    tell_time(Clock::Analog(1, 51, 30));
 }
