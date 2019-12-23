8e9776e6146b332fb65b5f4bfbeb6b9cbaf099cd
Abey Onalaja <Abey.onalaja@gmail.com>
2019-12-23 01:40:12 +0000

    flow

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,13 @@
-fn next_birthday(name: &str, current_age: u8) {
-    let next_age = current_age + 1;
-    println!(
-        "Hi {}, on your next birthday, you'll be {}!",
-        name, next_age
-    );
-}
-
-fn square(num: i32) -> i32 {
-    num * num
-}
-
 fn main() {
-    let mut x = 5;
-    let y = 6;
-    let z = x + y;
-
-    println!("z is {}", z);
-
-    x += 1;
-
-    println!("x is {}", x);
-
-    let a = true;
-    let b = false;
-
-    if a {
-        println!("a is true");
+    for i in 1..11 {
+        println!("Now serving number {}", i);
     }
 
-    if b {
-        println!("b is true");
+    let x = 3;
+    match x {
+        1 => println!("One is the loneliest number."),
+        2 => println!("Two's company"),
+        3 => println!("Three is a crowd"),
+        _ => println!("Some other number"),
     }
-
-    let tup = (1, 'c', true);
-    let first = tup.0;
-    let second = tup.1;
-
-    println!("the first is {} and the second is {}", first, second);
-    next_birthday("Bob", 34);
-    println!("The answer is {}", square(32));
 }
