ac043ed75ff76dd9b0d0392af1b80f8147b32e3b
Abey Onalaja <Abey.onalaja@gmail.com>
2019-12-26 03:19:30 +0000

    borrowing - person struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
+struct Person {
+    name: String,
+}
+
+fn congratulate(person: &Person) {
+    println!("Congratulations, {}!!!", person.name);
+}
+
 fn main() {
     let s = String::from("book");
     let p = pluralize(s.clone());
     println!("I have one {}, you have two {}", s, p,);
+
+    let ps = Person {
+        name: String::from("Abey"),
+    };
+    congratulate(&ps);
+    println!("Can still use ps here: {}", ps.name)
 }
 
 fn pluralize(word: String) -> String {
